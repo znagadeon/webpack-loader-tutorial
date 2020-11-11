5b2df21664f26c61f9a7b9b2da9fde4807f688cb
Kim Songhyun <yeonmu4121@gmail.com>
2020-11-11 14:41:05 +0900

    Use emitFile

--- loaders/html-loader.js
+++ loaders/html-loader.js
@@ -6,5 +6,13 @@ const getImagePaths = text => text.split('\n')
 	.map(match => match[1]);
 
 module.exports = function (source) {
+	const images = getImagePaths(source);
+	images.forEach(image => {
+		this.emitFile(
+			image,
+			this.fs.readFileSync(path.join(this.resourcePath, '..', image)),
+		);
+	});
+
 	return `module.exports = \`${source}\``;
 };
